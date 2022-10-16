794ff2d08ac1fda3e5365a99291b400d963d1635
mehdi marzban <marzban7272@gmail.com>
2022-10-16 23:48:01 +0330

    add all method decorators

--- src/decorators/routers.decorators.ts
+++ src/decorators/routers.decorators.ts
@@ -1,21 +1,49 @@
 import { Router } from "express";
 
-import {routerPathMaker} from "../utils/path";
+import { routerPathMaker } from "../utils/path";
 
 const DecoratorRouter: Router = Router();
 
-export function Controller(decoratorPath?: string) {
-    return function (target: any){
-        const path : string = routerPathMaker(decoratorPath, "/");
+export function Controller(decoratorPath?: string): Function {
+    return function (target: any): void {
+        const path: string = routerPathMaker(decoratorPath, "/");
         DecoratorRouter.use(path, DecoratorRouter);
-    }
-} 
+    };
+}
 
-export function Get(decoratorPath?: string) {
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor){
+export function Get(decoratorPath?: string): Function {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
         const path: string = routerPathMaker(decoratorPath, propertyKey);
         DecoratorRouter.get(path, descriptor.value); //* can use target[propertyKey] instead of descriptor.value
-    }
+    };
+}
+
+export function Post(decoratorPath?: string): Function {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
+        const path: string = routerPathMaker(decoratorPath, propertyKey);
+        DecoratorRouter.post(path, descriptor.value);
+    };
+}
+
+export function Put(decoratorPath?: string): Function {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
+        const path: string = routerPathMaker(decoratorPath, propertyKey);
+        DecoratorRouter.put(path, descriptor.value);
+    };
+}
+
+export function Patch(decoratorPath?: string): Function {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
+        const path: string = routerPathMaker(decoratorPath, propertyKey);
+        DecoratorRouter.patch(path, descriptor.value);
+    };
+}
+
+export function Delete(decoratorPath?: string): Function {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor): void {
+        const path: string = routerPathMaker(decoratorPath, propertyKey);
+        DecoratorRouter.delete(path, descriptor.value);
+    };
 }
 
 export default DecoratorRouter;
