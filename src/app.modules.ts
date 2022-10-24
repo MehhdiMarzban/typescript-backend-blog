e0e684b06f1b6a9211255662c493a859812df6e6
mehdi marzban <marzban7272@gmail.com>
2022-10-24 23:42:31 +0330

    fix error after run build

--- src/app.modules.ts
+++ src/app.modules.ts
@@ -2,6 +2,6 @@ import fs from "fs";
 
 import "./utils/mongoDBContection";
 
-//* auto import all of contorollers
+//* auto import all of contorollers [for fix error must import manually]
 fs.readdirSync(__dirname + "\\controllers").map(fileName => import(`./controllers/${fileName}`));
 
